0b565d7b4cf0fc5d035787716234cf4946321577
Vivian So <vso@thoughtworks.com>
2022-04-07 10:28:55 -0400

    Refactor to have the Paper component inside MainPage

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import { Paper, CssBaseline } from '@material-ui/core';
+import { CssBaseline } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/core/styles';
 import Header from 'App/common/Header';
 import Footer from 'App/common/Footer';
-import { theme, useStyle } from 'App/common/styles';
+import { theme } from 'App/common/styles';
 import MainPage from './pages/MainPage';
 
-const App = (props) => {
-  const classes = useStyle();
+const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Header />
-      <div className={classes.root}>
-        <Paper className={classes.paper}>
-          <MainPage />
-        </Paper>
-      </div>
+      <MainPage />
       <Footer />
     </ThemeProvider>
   );
--- src/App/pages/MainPage/MainPage.jsx
+++ src/App/pages/MainPage/MainPage.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import { Paper } from '@material-ui/core';
 
 import { FormContextProvider } from 'App/pages/MainPage/FormContext';
 import PersonalInfo from 'App/pages/MainPage/Components/PersonalInfo';
 import ContactInfo from 'App/pages/MainPage/Components/ContactInfo';
 import Confirmation from './Components/Confirmation';
 import StepTracker from 'App/common/StepTracker';
+import { useStyle } from 'App/common/styles';
 
 const steps = [
   { label: 'Personal Info', component: PersonalInfo },
@@ -12,6 +14,7 @@ const steps = [
 ];
 
 const MainPage = () => {
+  const classes = useStyle();
   const [activeStep, setActiveStep] = useState(0);
   const isLastStep = activeStep === steps.length;
 
@@ -26,19 +29,21 @@ const MainPage = () => {
   };
 
   return (
-    <React.Fragment>
-      <h1>Main Body is here</h1>
-      <FormContextProvider>
-        {isLastStep ? (
-          <Confirmation />
-        ) : (
-          <>
-            <StepTracker activeStep={activeStep} steps={steps} />
-            <ComponentToRender onNext={onNext} onBack={onBack} />
-          </>
-        )}
-      </FormContextProvider>
-    </React.Fragment>
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <h1>Main Body is here</h1>
+        <FormContextProvider>
+          {isLastStep ? (
+            <Confirmation />
+          ) : (
+            <>
+              <StepTracker activeStep={activeStep} steps={steps} />
+              <ComponentToRender onNext={onNext} onBack={onBack} />
+            </>
+          )}
+        </FormContextProvider>
+      </Paper>
+    </div>
   );
 };
 
